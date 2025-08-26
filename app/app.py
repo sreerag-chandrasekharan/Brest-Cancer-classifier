0d6204723e0fb97a64109db88ce6388e0dd4d0cb
sreerag <sreeragc7@gmail.com>
2025-08-27 00:36:06 +0200

    update path

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 
 # min and max values for the sliders
-with open("feature_stats.json", "r") as f:
+with open("app/feature_stats.json", "r") as f:
     FEATURE_STATS = json.load(f)
 
 def get_min_max_mean(key):
